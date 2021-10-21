29a11603c44204b8132bec608effa054dad1a349
lweine01 <laurarweiner@gmail.com>
2021-10-20 20:53:09 -0400

    Updated Testimonials page, pulled in font awesome icons, started projects page.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import selfie from '../../assets/images/laura.jpg'
+import selfie from '../../assets/images/laura.jpg';
 
 const styles = {
     sectionStyle: {
@@ -12,6 +12,11 @@ const styles = {
         border: 'solid 7px black',
         maxWidth: '100%',
         height: 'auto',
+    },
+    icon: {
+        color: 'white',
+        paddingRight: '15px',
+        fontSize: '75%',
     }
 }
 
@@ -19,12 +24,20 @@ function About() {
     return (
         <section style={styles.sectionStyle} className='row'>
             <div className='col-8'>
-                <h3>What's up, I'm Laura.</h3>
+                <h3>Hello there, I'm Laura.</h3>
                 <p>Welcome to my portfolio! I am a motivated and energetic full stack web developer who enjoys a challenge, problem solving and logical thinking. Quickly able to learn and retain information as shown through excelling at the  UNCC Coding Bootcamp. I have a diverse workplace experience in Human Resources and Hospital Administration with proven skills in leadership and collaboration.</p>
                 <p>Some of my skills include JavaScript,  React, HTML, CSS, NodeJS, MySQL, MongoDB, API’s, OOP, Test Driven Development, and Model View Controller</p>
                 <p>Fun facts: I am born and raised in Charlotte, NC, I also enjoy swimming, weight training, hiking and wine tasting with friends. </p>
+                <p>
+                <a href='https://www.linkedin.com/in/laurarweiner/' style ={styles.icon}>
+                <i class='fab fa-linkedin fa-4x'></i>
+                </a>
+                <a href='https://github.com/lweine01' style ={styles.icon}>
+                <i class='fab fa-github-square fa-4x'></i>
+                </a>
+                </p>
+
             </div>
-            {/* <div className='col-1'></div> */}
             <div className='col-4'>
                 <img src={selfie} alt="selfie of me" style={styles.selfie} />
             </div>
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import Ghostly from '../../assets/images/ghostly.png'
 
+// const styles = {
+//     projCard: {
+//         backgroundImage: `url(${Ghostly})`,
+//     }
+// }
 
 function Projects() {
     return (
-        <section  className='row'>
-            Hi
+        <section className='container'>
+            <div className='row'>
+                <div class='card'>
+                    <img src={Ghostly} class='card-img-top' alt='Ghostly Stories Screenshot' />
+                    <div class="card-body">
+                        <h5 class="card-title">Ghostly Stories</h5>
+                        <p class="card-text">A collaborative full-stack project using the MVC paradigm, RESTful APIs, user authentication, and MySQL and Sequelize for the database.</p>
+                        <a href='https://github.com/lweine01/workout-tracker'>
+                            <i class='fab fa-github-square fa-4x'></i>
+                        </a>
+                        <a href='https://github.com/lweine01/workout-tracker'>
+                            <i class='fab fas fa-link fa-4x'></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
